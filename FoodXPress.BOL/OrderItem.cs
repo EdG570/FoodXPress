d641411a96771efc7bfef8e4f6833e134395401b
EdG570 <cole570@hotmail.com>
2018-01-21 18:42:15 -0500

    Add OrderItems class to BOL

---
+++ FoodXPress.BOL/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿namespace FoodXPress.BOL
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public FoodItem FoodItem { get; set; }
+        public int FoodItemId { get; set; }
+        public int OrderId { get; set; }
+    }
+}
