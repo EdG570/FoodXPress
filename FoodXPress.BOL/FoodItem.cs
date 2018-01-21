0167dd0fc199608990136c94599d2840f74409ad
EdG570 <cole570@hotmail.com>
2018-01-21 18:37:49 -0500

    Add FoodItem class

---
+++ FoodXPress.BOL/FoodItem.cs
@@ -0,0 +1,19 @@
+﻿namespace FoodXPress.BOL
+{
+    public enum Category
+    {
+        Meal,
+        Beverage,
+        Side,
+        Appetizer,
+        Dessert
+    }
+
+    public class FoodItem
+    {
+        public int FoodItemId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+    }
+}
