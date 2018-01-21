edb71685555521ea36b84a57c9f8d12f088cc311
EdG570 <cole570@hotmail.com>
2018-01-21 18:32:45 -0500

    Inital Commit

---
+++ FoodXPress.DAL/Helpers/ConnectionStringHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace FoodXPress.DAL.Helpers
+{
+    public class ConnectionStringHelper
+    {
+        public static string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
