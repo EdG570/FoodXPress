83c7fdc884a9429b39c2ec769dfe1e9efdeaf4dc
EdG570 <cole570@hotmail.com>
2018-01-21 18:48:55 -0500

    Add Order & Table class

---
+++ FoodXPress.BOL/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FoodXPress.BOL
+{
+    public enum Status
+    {
+        Placed,
+        Ready,
+        Delivered,
+        Paid,
+        Hold,
+        Void
+    }
+
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Status Status { get; set; }
+        public int TableId { get; set; }
+    }
+}
---
+++ FoodXPress.BOL/Table.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodXPress.BOL
+{
+    public class Table
+    {
+        public int TableId { get; set; }
+        public int TableNumber { get; set; }
+    }
+}
